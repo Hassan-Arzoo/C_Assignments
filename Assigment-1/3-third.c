7e0b86ed5c30ce5f10cd6024d0d1c92b2d74e9ad
Hassan-Arzoo <132836269+Hassan-Arzoo@users.noreply.github.com>
2026-07-17 04:05:32 +0530

    Add files via upload

---
+++ Assigment-1/3-third.c
@@ -0,0 +1,10 @@
+/*
+Write a program to print "MySirG" on the screen.(Remember to print in double qoutes)
+*/
+#include<stdio.h>
+int main(){
+    printf("\"");
+    printf("MySirG");
+    printf("\"");
+    return 0;
+}
---
+++ Assigment-1/6-sixth.c
@@ -0,0 +1,14 @@
+/*
+Write a program to print "Teacher's day" on the screen. (Remember to print double and single qoutes)
+*/
+#include<stdio.h>
+int main(){
+    printf("\"");
+    printf("Teacher");
+    printf("\'");
+    printf("s");
+    printf(" ");
+    printf("Day");
+    printf("\"");
+    return 0;
+}
